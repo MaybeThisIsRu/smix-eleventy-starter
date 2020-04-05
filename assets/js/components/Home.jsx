15c77d5c80940c08c0a7f38796cc34fef793ad6c
Ru Singh <inbox@ruppalsingh.com>
2020-04-05 21:54:40 +0530

    Make the homepage slightly more presentable

--- assets/js/components/Home.jsx
+++ assets/js/components/Home.jsx
@@ -2,30 +2,16 @@ import React from "react";
 
 let Home = () => (
 	<div className="flex flex-col">
-		<h2>Hi, I'm a starter for an API based React app.</h2>
-		<div className="flex flex-row">
-			<div className="w-1/4 h-60 pb-6 pr-6">
-				<img
-					className="w-auto h-full rounded-sm"
-					src="https://source.unsplash.com/350x350/?laptop,magazine,desk"
-				/>
-			</div>
-			<div className="w-3/4">
-				<p>
-					The best thing about me? You can look at the gulp source and
-					understand what's up. Oh no, that's not the best part - you
-					can make any amends with ease because it's easy to
-					understand all these tasks! Take that,{" "}
-					<code>create-react-app</code>!
-				</p>
-				<p>
-					We have TailwindCSS with sane-ish defaults. Delete about
-					three css files for a clean slate... and boom - you can
-					start your next big project without worrying about design
-					growth and muddy CSS.
-				</p>
-			</div>
+		<div className="h-60 w-full pb-6 pr-6">
+			<img
+				className="w-full h-auto rounded-sm"
+				src="https://source.unsplash.com/2560x700/?laptop,magazine,desk"
+			/>
 		</div>
+		<h2 className="">Hi, I'm a starter for an API based React app.</h2>
+		<p>
+			Please see the <a href="https://github.com/hirusi/react-on-gulp-starter/blob/develop/README.md" target="_blank" rel="noreferrer noopener">readme</a> on GitHub for more details.
+		</p>
 	</div>
 );
 
