32570f835de9c4997339bdb8b63d1842d8c3db13
Ru Singh <inbox@ruppalsingh.com>
2020-04-30 19:59:45 +0530

    Fix byTag filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,9 @@
 module.exports = function(config) {
-    const filters = require("./ssg/filters");
-    const shortcodes = require("./ssg/shortcodes");
-    const utils = require("./ssg/utils");
+	const filters = require("./ssg/filters");
+	const shortcodes = require("./ssg/shortcodes");
+	const utils = require("./ssg/utils");
 
-    const isProduction = process.env.NODE_ENV === 'production' ? true : false;
+	const isProduction = process.env.NODE_ENV === "production" ? true : false;
 
 	// *** Misc Options
 	// Additional files to watch for changes
@@ -19,22 +19,24 @@ module.exports = function(config) {
 	// Copy as-is from root to output path
 	config.addPassthroughCopy("admin");
 
-    // *** Plugins
-    // Nothing here right now.
+	// *** Plugins
+	// Nothing here right now.
 
-    // *** Shortcodes
-    // Jekyll replacement for post_url tag as an 11ty shortcode
-    config.addShortcode("getUrl", shortcodes.postUrl);
-    config.addShortcode("isOldPost", shortcodes.isOldPost);
+	// *** Shortcodes
+	// Jekyll replacement for post_url tag as an 11ty shortcode
+	config.addShortcode("getUrl", shortcodes.postUrl);
+	config.addShortcode("isOldPost", shortcodes.isOldPost);
 
-    // *** Filters
-    // Dates
-    config.addFilter("friendlyDate", filters.friendlyDate);
+	// *** Filters
+	// Dates
+	config.addFilter("friendlyDate", filters.friendlyDate);
 	config.addFilter("dateInISO8601", filters.dateInISO8601);
-	
+	// Filter specified collection by tag
+	config.addFilter("byTag", filters.byTag);
+
 	// *** Internal filter functions
 	// const liveItems = item => item.date <= new Date();
-	const publishedItems = item => isProduction ? !item.data.draft : true;
+	const publishedItems = item => (isProduction ? !item.data.draft : true);
 
 	// *** Collections
 	// Blog posts
@@ -45,14 +47,14 @@ module.exports = function(config) {
 			.reverse();
 	});
 
-    return {
-        pathPrefix: '/', // useful for GitHub pages
-        dir: {
-            input: "./",
-            output: "./dist",
-            includes: "includes",
-            layouts: "layouts",
-            data: "data"
-        },
-    };
-}
+	return {
+		pathPrefix: "/", // useful for GitHub pages
+		dir: {
+			input: "./",
+			output: "./dist",
+			includes: "includes",
+			layouts: "layouts",
+			data: "data"
+		}
+	};
+};
