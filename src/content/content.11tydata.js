05d805dcf13908c052aa4b58041f887c16c7c682
hirusi <inbox@rusingh.com>
2020-08-10 11:16:06 +0530

    design: layout updates to better accomodate new author card

--- src/content/content.11tydata.js
+++ src/content/content.11tydata.js
@@ -1,15 +1,24 @@
 const env = require("../../eleventy/env");
 
+/**
+ *
+ * @param {object} data
+ * @returns {string|false}
+ */
+const getPermalink = data => {
+	return env.is11tyProduction
+		? data.draft
+			? false
+			: data.permalink
+		: data.permalink;
+};
+
 module.exports = () => {
 	return {
 		eleventyComputed: {
-			permalink: data => {
-				return env.is11tyProduction
-					? data.draft
-						? false
-						: data.permalink
-					: data.permalink;
-			}
+			permalink: getPermalink,
+			eleventyExcludeFromCollections: data =>
+				getPermalink(data) ? false : true
 		}
 	};
 };
