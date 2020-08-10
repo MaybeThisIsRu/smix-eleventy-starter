a81ad7ee5f61f9ff170e3d2dfbbabbc42eeceaf7
hirusi <inbox@rusingh.com>
2020-08-10 13:07:39 +0530

    fix: default node dev env to true if no env set

--- eleventy/env.js
+++ eleventy/env.js
@@ -1,6 +1,8 @@
 module.exports = {
 	isNodeProduction: process.env.NODE_ENV === "production",
-	isNodeDevelopment: process.env.NODE_ENV === "development",
+	isNodeDevelopment: process.env.NODE_ENV
+		? process.env.NODE_ENV === "development"
+		: "development",
 	is11tyProduction: process.env.ELEVENTY_ENV === "production",
 	is11tyStaging: process.env.ELEVENTY_ENV === "staging"
 };
