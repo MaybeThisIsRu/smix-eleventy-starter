434a9ce76700bc0331795d0ea1a1cb901a1961f6
hirusi <hello@rusingh.com>
2022-02-16 19:57:03 +0530

    Remove staging build command
    This further removes Forestry integration.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -24,13 +24,4 @@ const develop = series(
 	)
 );
 
-// Staging is used for Forestry CMS
-// Builds assets once
-// Watch for img changes
-// Start SSG in watch mode with built-in server, ideally incremental building
-const staging = series(
-	parallel(css, js, img, font, html),
-	parallel(imgWatcher, eleventyWatch)
-);
-
-export { production, staging, develop };
+export { production, develop };
